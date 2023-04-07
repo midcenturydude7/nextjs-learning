bf74724ce3f3cb69ac37f1a6b0dd5efbeb9e0dd2
midCenturyDude7 <mgriffes@gmail.com>
2023-04-06 20:19:41 -0700

    Implemented data fetching, dynamic routing and dynamic metadata

---
+++ next03/app/users/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import type { Metadata } from "next";
+import getAllUsers from "@/lib/getAllUsers";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Users",
+  description: "This is the users page",
+};
+
+export default async function UsersPage() {
+  const usersData: Promise<User[]> = getAllUsers();
+
+  const users = await usersData;
+
+  console.log("Hello");
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">Back to Home</Link>
+        <br />
+        {users.map((user) => {
+          return (
+            <>
+              <p key={user.id} className="users">
+                <Link href={`/users/${user.id}`}>{user.name}</Link>
+              </p>
+            </>
+          );
+        })}
+      </h2>
+    </section>
+  );
+
+  return content;
+}
